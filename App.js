12be81141ea1f7d0a44bd9b3a17c34bf04efbf9e
Affan-97 <affana959@gmail.com>
2024-05-24 17:02:03 +0700

    feat: add env

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { API_KEY } from '@env';
 
 export default function App() {
+console.log(API_KEY);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
